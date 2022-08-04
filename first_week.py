d95c41d2894afa0b4018e30c35b6c3cdae8d8686
eddie <popokbs@gmail.com>
2022-08-04 16:18:47 +0900

    firstweek mission

---
+++ first_week.py
@@ -0,0 +1,9 @@
+#### 한국 나이 구하기
+
+# 1. 간단한 코드
+
+kor_age = int(input("한국나이를 입력해주세요 : "))
+birth = int(input("생일이 지났습니까? 지났으면 0 아니면 -1 : "))
+
+internation_age = kor_age -1 + birth
+print(internation_age)

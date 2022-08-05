e96b6619531af48a14bbe6920e14e2fd3d22ad5c
eddie <popokbs@gmail.com>
2022-08-05 23:29:27 +0900

    txt 파일 생성

--- 4th_week2.py
+++ 4th_week2.py
@@ -13,15 +13,32 @@
 # count_word(a, "습니다")
 # 2
 
-def count_word(a, word):
+def create_txt(a):
     with open("4th_week2.txt","w") as f:
-        f.write()
+        f.write(a)
 
-    lines = f.readlines()
-    for line in lines:
-        print(line)
+def count_word(a, word):
+    with open("4th_week.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            print(line)
 
-count_word(a, "습니다")
+a = """중국과 타이완의 군사적 긴장이 최고조에 달하고 있습니다.
+
+중국은 타이완 상공을 가로질러 미사일을 발사했고, 예고한 군사 훈련에 최첨단 스텔스기 등 100여 대의 항공기도 동원했습니다.
+
+이 과정에서 일본 해역에도 미사일이 떨어져 긴장은 더욱 커지고 있습니다.
+
+국제부 뉴스룸을 연결합니다. 이승훈 기자!
+
+중국군이 낸시 펠로시 미국 하원의장의 타이완 방문에 대한 보복으로 미사일 발사를 강행했죠?
+
+[기자]
+중국 인민해방군이 타이완의 동서남북에 장거리포와 미사일을 발사했습니다.
+
+이른바, '중요 군사 훈련 실탄사격' 첫날 미사일이 떨어진 곳은 이미 예고한 타이완 주변 6개 구역입니다."""
+create_txt(a)
+# count_word(a, "습니다")
 # count = 0
 
 # for i in a :

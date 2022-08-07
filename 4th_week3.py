1a1f13b0081d552d11ce4e04137c5f329e882812
eddie <popokbs@gmail.com>
2022-08-07 23:12:40 +0900

    정규표현식으로 코드 변경

--- 4th_week3.py
+++ 4th_week3.py
@@ -28,26 +28,56 @@
 # 잘못 쓴 사람: 최정
 # 잘못 쓴 번호: 111-1111-1111
 
+import re
 
-def wrong_guest_book(guest_book):
+def write_guest_book(guest_book): # 방명록을 먼저 txt 파일로 만들기
     with open("guest_book.txt","w") as f:
         f.write(guest_book)
-    with open("guest_book.txt","r") as f:
-        words = f.readlines()
-    wrong = []
-    for i in words:
-        result = i.find("010-",-13,-1)
-        if result == -1 :
-            wrong.append(i)
-        
-    for a in wrong:
-        print(f"잘못 쓴 사람 : {a[:2]}\n잘못 쓴 번호 : {a[3:]}")
-            
-            
+def wrong_guest_book(guest_book): 
+    with open("guest_book.txt", "r") as f:
+        a = f.readlines() # 한줄씩 받아와서 a에 담아줌
+        for i in a :
+            check = re.search("(010)-\d{4}-\d{4}",i) #전화번호 형식에 맞게 정규표현식 사용
+            if not check: # 형식에 맞지 않는 방명록 찾기
+                wrong = re.compile("\W+")
+                result = wrong.split(i) # 정규식에 맞게 split 해준뒤
+                name = result[0] # result에서 이름값만 name에 담기
+                remove_name = re.findall("\w+\,", i) 
+                number = i.replace(remove_name[0],"")# result 값에서 이름값만 지우고 number에 담기
+                print(f"잘못 쓴 사람 : {name}\n잘못 쓴 번호 : {number}")
+                
+            else :
+                pass
+
 guest_book = """김갑,123456789
 이을,010-1234-5678
 박병,010-5678-111
 최정,111-1111-1111
 정무,010-3333-3333"""
 
+write_guest_book(guest_book)
 wrong_guest_book(guest_book)
+
+
+# def wrong_guest_book(guest_book):
+#     with open("guest_book.txt","w") as f:
+#         f.write(guest_book)
+#     with open("guest_book.txt","r") as f:
+#         words = f.readlines()
+#     wrong = []
+#     for i in words:
+#         result = i.find("010-",-13,-1)
+#         if result == -1 :
+#             wrong.append(i)
+        
+#     for a in wrong:
+#         print(f"잘못 쓴 사람 : {a[:2]}\n잘못 쓴 번호 : {a[3:]}")
+            
+            
+# guest_book = """김갑,123456789
+# 이을,010-1234-5678
+# 박병,010-5678-111
+# 최정,111-1111-1111
+# 정무,010-3333-3333"""
+
+# wrong_guest_book(guest_book)

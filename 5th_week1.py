8aee7c638a9a9375e7943223f1670376ef496900
eddie <popokbs@gmail.com>
2022-08-12 20:34:59 +0900

    1번 미션 승리조건 추가 완료

--- 5th_week1.py
+++ 5th_week1.py
@@ -6,16 +6,26 @@ def bs31():
     while True:
         my = list(map(int, input("My turn - 숫자를 입력하세요(최대 3개): ").split())) # 입력값을 int ,list 로 바로 변환
         
-        if len(my) <= 3 and my == game[:len(my)]: # 입력값이 3개 이하이고 입력값과 남은 숫자들을 매치해서 잘못된 숫자가 없는지 체크
+        if game[0] == 31:
+            print("컴퓨터 승리")
+            quit()
+        elif len(my) <= 3 and my == game[:len(my)]: # 입력값이 3개 이하이고 입력값과 남은 숫자들을 매치해서 잘못된 숫자가 없는지 체크
             for i in my:
                 game.remove(i) # 차례로 남은 숫자들을 제거
-            print(f"현재 숫자 : {game[0]-1}") # 현재숫자는 마지막으로 제거한 숫자임으로 -1을 해준다
-
+            try:
+                print(f"현재 숫자 : {game[0]-1}") # 현재숫자는 마지막으로 제거한 숫자임으로 -1을 해준다
+            except:
+                print("컴퓨터 승리")
+                quit()
             com = random.randint(1,3) # 1~3까지 3가지의 랜덤한 숫자를 생성
             while com: # com 값이 0일때 while문을 탈출하도록 함
-                print(f"컴퓨터 : {game[0]}")
-                game.remove(game[0])
-                com -= 1
+                if game[0] == 31:
+                    print("플레이어 승리!")
+                    quit()
+                else:
+                    print(f"컴퓨터 : {game[0]}")
+                    game.remove(game[0])
+                    com -= 1
             print(f"현재 숫자 : {game[0]-1}\n")
         else :
             print("입력값이 잘못되었습니다\n")

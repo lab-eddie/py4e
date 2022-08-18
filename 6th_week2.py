85336cd6f1576807ce0916919c97a05f6873d75e
eddie <popokbs@gmail.com>
2022-08-19 08:08:42 +0900

    6주차 2번미션

--- 6th_week2.py
+++ 6th_week2.py
@@ -15,3 +15,8 @@ member_names = ["갑돌이", "갑순이", "을돌이", "을순이", "병돌이",
 member_records = [[4,5,3,5,6,5,3,4,1,3,4,5],[2,3,4,3,1,2,0,3,2,5,7,2], 
            [1,3,0,3,3,4,5,6,7,2,2,1],[3,2,9,2,3,5,6,6,4,6,9,9],
            [8,7,7,5,6,7,5,8,8,6,10,9],[7,8,4,9,5,10,3,3,2,2,1,3]]
+
+members_list = {member_names[i]: member_records[i] for i in range(len(member_names))}
+
+for name in members_list.keys():
+    print(name)
